55c99fe5c224f2f1053172d0bdef73394dff1978
pengzhaoshi <286943802@qq.com>
2019-08-21 14:43:35 +0800

    提交测试

---
+++ src/com/pzs/test/Hello.java
@@ -0,0 +1,9 @@
+package com.pzs.test;
+
+public class Hello {
+	
+	private void sysou() {
+		// TODO Auto-generated method stub
+		System.out.println("when i was young i listen to my radio");
+	}
+}
